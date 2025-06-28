4ec66de9b74c12b0813b40b434249daf4aae3bca
Abhaya-Shresthaa <gopalkumarshrestha123@gmail.com>
2025-06-28 23:36:17 +0545

    add all

--- Lab 5 DoublyLL/dll.cpp
+++ Lab 5 DoublyLL/dll.cpp
@@ -2,129 +2,52 @@
 using namespace std;
 
 class Node{
-    public:
+  public:
     Node* next;
     Node* prev;
-    int data;
-    Node(int val): data(val), next(NULL){}
+    int data;  
+    Node(int val){
+        next = prev = NULL;
+        data = val;
+    }
 };
 
-class DoublyList{
+class List{
     public:
-    Node* head;
-    Node* tail;
+        Node* head;
 
-    DoublyList(){
-        head = tail = NULL;
-    }
-
-    void pushFront(int val){
-        Node* newNode = new Node(val);
-
-        if(head == NULL){
-            head = tail =newNode;
+        List(){
+            head = NULL;
         }
-        else {
-            newNode-> next = head;
-            head ->prev = newNode;
-            head = newNode;
-        }
-    }
 
-    void pushBack(int val){
-        Node* newNode = new Node(val);
-        Node* temp = head;
-        if (head == NULL){
-            head = tail = newNode;
-        }
-        else{
-            newNode -> next = NULL;
-            while(temp -> next != NULL){
-                temp = temp -> next;
+        void pushFront(int val){
+            Node* newNode = new Node(val);
+            if (head == NULL){
+                head = newNode;
             }
-            newNode -> prev = temp;
-            temp ->next = newNode;
-            tail = newNode;
-
-        }
-    }
-
-    void popFront(){
-        if(head == NULL){
-            cout << "List is empty" << endl;
-            return;
-        }
-        
-        Node* temp = head;
-        head = head -> next;
-
-        if(head != NULL){
-            head ->prev = NULL;
+            else{
+                newNode -> next = head;
+                head -> prev = newNode;
+                head = newNode;
+            }
         }
 
-        temp ->next = NULL;
-        delete temp;
-        
-    }
-
-    // without using tail 
-    void popBack(){
-        if(head == NULL){
-            cout << "List is empty" << endl;
-            return;
+        void pushBack(int val){
+            Node* newNode = new Node(val);
+            if (head == NULL){
+                head = newNode;
+            }
+            else{
+                Node* temp = head;
+                while(temp -> next != NULL){
+                    temp = temp -> next;
+                }
+            }
         }
-
-        Node* temp = head;
-        Node* prev = NULL;
-        while((temp->next) != NULL){
-            temp = temp->next;
-        }
-        prev = temp -> prev;
-        prev -> next = NULL;
-        temp ->prev = NULL;
-        delete temp;
-    }
-
-    // using tail
-
-    void usingTailPopBack(){
-
-        if(head == NULL){
-            cout << "List is empty" << endl;
-            return;
-        }
-
-        Node* temp = tail;
-        tail = tail-> prev;
-        if(tail != NULL){
-            tail -> next = NULL;
-        }
-        temp -> prev = NULL;
-        delete temp;
-
-
-    }
-
-    void display(){
-        Node* temp = head;
-        while(temp != NULL){
-            cout << temp->data << " <=> " ;
-            temp = temp ->next;
-        }
-        cout << "NULL" << endl;
-    }
 };
 
 int main(){
-    DoublyList ll;
-    ll.pushFront(10);
-    ll.pushFront(20);
-    ll.pushFront(30);
-    ll.pushBack(40);
-    ll.pushBack(50);
-    ll.popBack();
 
-    ll.display();
 
     return 0;
 }
