bc2c475eda22ee2882ea65fa94a7d33bdd7ab341
Abhaya-Shresthaa <gopalkumarshrestha123@gmail.com>
2025-06-29 01:20:41 +0545

    polynomial

---
+++ Lab 5 DoublyLL/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+ // void addVal(int co, int po){
+        //     Node* newNode = new Node(co, po);
+        //     if(head == NULL){
+        //         head = newNode;
+        //         return;
+        //     }
+        //     else{
+        //         newNode ->next = head;
+        //         head = newNode;
+        //     }
+        // }
